98c7e7585c48c5f1dc48925676e8cfbe69921b7c
Egor Badmaev <badmaeve2511@gmail.com>
2021-03-15 18:31:59 +0300

    feat: add base imports and variables (constants)

---
+++ main.py
@@ -0,0 +1,13 @@
+import httplib2
+import apiclient.discovery
+from oauth2client.service_account import ServiceAccountCredentials
+import tokens
+
+CREDENTIALS_FILE = 'creds.json'
+spreadsheet_id = tokens.spreadsheet_id
+credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    CREDENTIALS_FILE,
+    ['https://www.googleapis.com/auth/spreadsheets',
+     'https://www.googleapis.com/auth/drive'])
+httpAuth = credentials.authorize(httplib2.Http())
+service = apiclient.discovery.build('sheets', 'v4', http=httpAuth)
