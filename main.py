19d8a26caf8efa0cd0fdef943507c5bfa017dcb8
Egor Badmaev <badmaeve2511@gmail.com>
2021-03-16 19:02:55 +0300

    feat: add main app logic

--- main.py
+++ main.py
@@ -3,11 +3,36 @@ import apiclient.discovery
 from oauth2client.service_account import ServiceAccountCredentials
 import tokens
 
-CREDENTIALS_FILE = 'creds.json'
-spreadsheet_id = tokens.spreadsheet_id
-credentials = ServiceAccountCredentials.from_json_keyfile_name(
-    CREDENTIALS_FILE,
-    ['https://www.googleapis.com/auth/spreadsheets',
-     'https://www.googleapis.com/auth/drive'])
-httpAuth = credentials.authorize(httplib2.Http())
-service = apiclient.discovery.build('sheets', 'v4', http=httpAuth)
+# CREDENTIALS_FILE = 'creds.json'
+# spreadsheet_id = tokens.spreadsheet_id
+# credentials = ServiceAccountCredentials.from_json_keyfile_name(
+#     CREDENTIALS_FILE,
+#     ['https://www.googleapis.com/auth/spreadsheets',
+#      'https://www.googleapis.com/auth/drive'])
+# httpAuth = credentials.authorize(httplib2.Http())
+# service = apiclient.discovery.build('sheets', 'v4', http=httpAuth)
+stdin1 = input()  # 8:15 - 10:05
+# stdin2 = input()  # 14:40 - 16:00
+# stdin3 = input()  # 10:55 - 13:25
+
+
+def main(value):
+    start_time_hours = int(value.split(' - ')[0].split(':')[0])  # получаю значение часа
+    start_time_minutes = int(value.split(' - ')[0].split(':')[1])  # получаю значение минут
+    end_time_hours = int(value.split(' - ')[1].split(':')[0])  # Аналогично
+    end_time_minutes = int(value.split(' - ')[1].split(':')[1])  # Аналогично
+    # 8:15 - 10:05 => '8:15', '10:05' => 8, 15, 10, 5
+    diff_hours = end_time_hours - start_time_hours
+    diff_minutes = end_time_minutes - start_time_minutes
+    if diff_minutes < 0:
+        diff_hours -= 1
+        diff_minutes = 60 + diff_minutes  # 60 + (-10) = 50
+    while diff_hours > 0:
+        diff_minutes += 60
+        diff_hours -= 1
+    amount_of_hours = round(diff_minutes / 60, 2)
+    print(amount_of_hours)
+
+main(stdin1)
+# main(stdin2)
+# main(stdin3)
