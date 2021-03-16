541325e65f70c170939d89e560ae522e57614f91
Egor Badmaev <badmaeve2511@gmail.com>
2021-03-16 19:03:57 +0300

    refactor: remove useless strings of code

--- main.py
+++ main.py
@@ -3,17 +3,15 @@ import apiclient.discovery
 from oauth2client.service_account import ServiceAccountCredentials
 import tokens
 
-# CREDENTIALS_FILE = 'creds.json'
-# spreadsheet_id = tokens.spreadsheet_id
-# credentials = ServiceAccountCredentials.from_json_keyfile_name(
-#     CREDENTIALS_FILE,
-#     ['https://www.googleapis.com/auth/spreadsheets',
-#      'https://www.googleapis.com/auth/drive'])
-# httpAuth = credentials.authorize(httplib2.Http())
-# service = apiclient.discovery.build('sheets', 'v4', http=httpAuth)
-stdin1 = input()  # 8:15 - 10:05
-# stdin2 = input()  # 14:40 - 16:00
-# stdin3 = input()  # 10:55 - 13:25
+CREDENTIALS_FILE = 'creds.json'
+spreadsheet_id = tokens.spreadsheet_id
+credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    CREDENTIALS_FILE,
+    ['https://www.googleapis.com/auth/spreadsheets',
+     'https://www.googleapis.com/auth/drive'])
+httpAuth = credentials.authorize(httplib2.Http())
+service = apiclient.discovery.build('sheets', 'v4', http=httpAuth)
+stdin = input()
 
 
 def main(value):
@@ -33,6 +31,5 @@ def main(value):
     amount_of_hours = round(diff_minutes / 60, 2)
     print(amount_of_hours)
 
-main(stdin1)
-# main(stdin2)
-# main(stdin3)
+
+main(stdin)
