f1a88f56a156ada4b0d574d30abc8a00ca2556aa
Egor Badmaev <badmaeve2511@gmail.com>
2021-03-16 19:23:30 +0300

    refactor: add modules for spreadsheet

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-import httplib2
-import apiclient.discovery
-from oauth2client.service_account import ServiceAccountCredentials
-import tokens
-
-CREDENTIALS_FILE = 'creds.json'
-spreadsheet_id = tokens.spreadsheet_id
-credentials = ServiceAccountCredentials.from_json_keyfile_name(
-    CREDENTIALS_FILE,
-    ['https://www.googleapis.com/auth/spreadsheets',
-     'https://www.googleapis.com/auth/drive'])
-httpAuth = credentials.authorize(httplib2.Http())
-service = apiclient.discovery.build('sheets', 'v4', http=httpAuth)
-
-
 def main(value):
     start_time_hours = int(value.split(' - ')[0].split(':')[0])  # получаю значение часа
     start_time_minutes = int(value.split(' - ')[0].split(':')[1])  # получаю значение минут
